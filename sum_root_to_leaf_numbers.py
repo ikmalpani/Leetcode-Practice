15d87db0db55c7978f38803434e28e61d5e14aaa
Keshav Malpani <ikmalpani@users.noreply.github.com>
2018-03-15 16:29:35 -0500

    Adding solutions 3/15

---
+++ sum_root_to_leaf_numbers.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumNumbers(self, root):
+        if not root:
+            return 0
+        stack, res = [(root, root.val)], 0
+        print (stack, res)
+        while stack:
+            node, value = stack.pop()
+            if node:
+                if not node.left and not node.right:
+                    res += value
+                if node.right:
+                    stack.append((node.right, value*10+node.right.val))
+                if node.left:
+                    stack.append((node.left, value*10+node.left.val))
+        return res
